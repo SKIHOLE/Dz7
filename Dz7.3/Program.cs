9fbcd00f253ee06b267e235b0afe7c0254fe47ae
vanshot super <vanya12345654@gmail.com>
2023-06-01 13:41:26 +0300

    D

--- Dz7.3/Program.cs
+++ Dz7.3/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace Dz7._3
 {
-    internal class Program
+    public class MyClass
     {
-        static void Main(string[] args)
+        public string change;
+    }
+
+    public struct MyStruct
+    {
+        public string change;
+    }
+
+    public class Program
+    {
+        static void ClassTaker(MyClass myClass)
         {
+            myClass.change = "змінено";
+        }
+
+        static void StructTaker(ref MyStruct myStruct)
+        {
+            myStruct.change = "змінено";
+        }
+
+        static void Main()
+        {
+            MyClass myClass = new MyClass();
+            myClass.change = "не змінено";
+
+            MyStruct myStruct = new MyStruct();
+            myStruct.change = "не змінено";
+
+            Console.WriteLine("Початкові значення:");
+            Console.WriteLine("MyClass: " + myClass.change);
+            Console.WriteLine("MyStruct: " + myStruct.change);
+
+            ClassTaker(myClass);
+            StructTaker(ref myStruct);
+
+            Console.WriteLine("\nЗначення після виклику методів:");
+            Console.WriteLine("MyClass: " + myClass.change);
+            Console.WriteLine("MyStruct: " + myStruct.change);
+            Console.ReadKey();
         }
     }
 }
