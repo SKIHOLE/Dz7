963092001d17f4e62b1ef7f8ffcf95cfa15f89e1
vanshot super <vanya12345654@gmail.com>
2023-06-01 13:59:18 +0300

    S

---
+++ Dz7.5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz7._5
+{
+    struct Notebook
+    {
+        public string model;
+        public string manufacturer;
+        public double price;
+
+        public Notebook(string model, string manufacturer, double price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Модель: " + model);
+            Console.WriteLine("Виробник: " + manufacturer);
+            Console.WriteLine("Вартість: " + price);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Notebook myNotebook = new Notebook("Asus ZenBook", "Asus", 1500.99);
+            myNotebook.DisplayInfo();
+        }
+    }
+}
